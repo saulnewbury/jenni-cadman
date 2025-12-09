df41d6c44137056326e917d43defe4ebe8b31671
saulnewbury <saulnewbury@gmail.com>
2025-12-09 10:50:44 +0700

    Fixed line-height of address on bio page

--- src/pages/Bio/Bio.js
+++ src/pages/Bio/Bio.js
@@ -79,6 +79,8 @@ const Bio = () => {
 
       // Credentials
       const credsPs = creds.current.children
+
+      gsap.set(creds.current, { lineHeight: '1.3em' })
       const credsChildSplit = new SplitText(credsPs, {
         type: 'lines',
         linesClass: 'split-child'
@@ -101,7 +103,7 @@ const Bio = () => {
 
       // Titles
       const titles = gsap.utils.toArray(qE('h2'))
-      titles.forEach(title => {
+      titles.forEach((title) => {
         gsap.fromTo(
           title,
           { y: 20, opacity: 0 },
@@ -119,7 +121,7 @@ const Bio = () => {
 
       // Dates
       const dates = gsap.utils.toArray(qE('.date'))
-      dates.forEach(date => {
+      dates.forEach((date) => {
         gsap.fromTo(
           date,
           { y: 20, opacity: 0 },
@@ -137,7 +139,7 @@ const Bio = () => {
 
       // Details
       const details = gsap.utils.toArray(qE('.details'))
-      details.forEach(details => {
+      details.forEach((details) => {
         gsap.fromTo(
           details,
           { y: 20, opacity: 0 },
@@ -160,21 +162,21 @@ const Bio = () => {
   }, [])
 
   return (
-    <div ref={bio} className="bio">
-      <div className="bio-inner">
-        <section className="jenni-cadman indent">
-          <div className="image">
-            <div className="image-wrapper">
-              <div ref={overlay} className="overlay"></div>
+    <div ref={bio} className='bio'>
+      <div className='bio-inner'>
+        <section className='jenni-cadman indent'>
+          <div className='image'>
+            <div className='image-wrapper'>
+              <div ref={overlay} className='overlay'></div>
               <img
                 ref={image}
-                src="/images/other/jenni-cadman-textileartist-portrait.webp"
-                alt=""
+                src='/images/other/jenni-cadman-textileartist-portrait.webp'
+                alt=''
               />
             </div>
           </div>
-          <div className="credentials">
-            <div ref={creds} className="credentials-inner">
+          <div className='credentials'>
+            <div ref={creds} className='credentials-inner'>
               <p>
                 Jenni Cadman <br />
                 Textile artist <br />
@@ -202,33 +204,33 @@ const Bio = () => {
               <p/> */}
             </div>
           </div>
-          <div className="info">
-            <div ref={info} className="info-inner">
+          <div className='info'>
+            <div ref={info} className='info-inner'>
               {bioInfo.map((para, idx) => (
                 <p key={idx.toString()}>{para}</p>
               ))}
             </div>
           </div>
         </section>
-        <section ref={experience} className="experience indent">
+        <section ref={experience} className='experience indent'>
           {resumeEntries.map((obj, idx) => {
             return (
               <div
                 key={idx.toString()}
                 className={`category ${idx % 2 === 0 ? '' : 'fill'}`}
               >
-                <div className="heading">
-                  <h2 className="uppercase sm">{obj.title}</h2>
+                <div className='heading'>
+                  <h2 className='uppercase sm'>{obj.title}</h2>
                 </div>
                 <ul>
                   {obj.entries.map((entry, idx) => {
                     return (
                       <li key={idx.toString()}>
-                        <div className="date">
+                        <div className='date'>
                           <span>{entry.date}</span>
                         </div>
-                        <div className="details">
-                          <span className="info">{entry.info}</span>{' '}
+                        <div className='details'>
+                          <span className='info'>{entry.info}</span>{' '}
                           {entry.location}
                         </div>
                       </li>
@@ -239,7 +241,7 @@ const Bio = () => {
             )
           })}
         </section>
-        <div className="copyright">Jenni Cadman &#169; 2022</div>
+        <div className='copyright'>Jenni Cadman &#169; 2022</div>
       </div>
     </div>
   )
